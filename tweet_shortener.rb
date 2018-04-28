146131cb63e8b22753eebec2960f28306e2a3fc5
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-28 00:04:54 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,100 @@
 # Write your code here.
+
+def dictionary
+  
+  word_changes = { 
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+
+def word_substituter (tweet)
+  
+  tweet_array = tweet.split(" ")
+  changes_hash_keys = dictionary.keys
+  changes_hash_values = dictionary.values
+  updated_tweet_array = []
+  the_index = nil
+  
+    tweet_array.map do |word|
+        if changes_hash_keys.include?(word.downcase)
+          the_index = changes_hash_keys.index {|i| i.include?(word.downcase)}
+          updated_tweet_array << changes_hash_values[the_index]
+        else
+          updated_tweet_array << word
+        end
+    end
+    updated_tweet_array.join(" ")
+    
+end
+
+
+def bulk_tweet_shortener (tweet_list)
+  
+    tweet_list.each do |message|
+      puts word_substituter(message)
+    end  
+end
+
+
+
+def selective_tweet_shortener (tweet)
+  
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+  
+end
+
+
+
+def shortened_tweet_truncator (tweet)
+  
+  if tweet.length > 140
+
+      new_tweet = word_substituter(tweet)
+      
+        if new_tweet.length > 140
+          new_tweet[0..136] + "..."
+        else
+          new_tweet
+        end
+  else 
+    tweet
+      
+  end 
+  
+end  
+      
+      
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
